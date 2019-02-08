248ace300e72cc6256fdf6fd91a9eb8e50150c65
dgorbach <daniel.gorbach@gmail.com>
2019-02-08 08:45:39 +0100

    Kuerzel Auszubildend mit Tests absichern

---
+++ tests/Vodamep.Tests/Data/QualificationCodeProviderTests.cs
@@ -0,0 +1,20 @@
+﻿using Vodamep.Data;
+using Xunit;
+
+namespace Vodamep.Tests.Data
+{
+    public class QualificationCodeProviderTests
+    {
+        [Fact]
+        public void Trainee_IsValidCode()
+        {
+            Assert.True(QualificationCodeProvider.Instance.IsValid(QualificationCodeProvider.Instance.Trainee));
+        }
+
+        [Fact]
+        public void Trainee_DescriptionIsAuszubildend()
+        {
+            string.Equals("Auszubildend", QualificationCodeProvider.Instance.Values[QualificationCodeProvider.Instance.Trainee]);
+        }
+    }
+}
