3d6142c69c59ae87f99e1be5ddbe458b13c44dd2
Markus Vinzenz <mv1@alpencode.net>
2020-12-19 13:30:52 +0100

    SpecFlow Validations

---
+++ src/Vodamep/Data/Dummy/AgpDataGeneratorAgpReportExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using Vodamep.Agp.Model;
+
+
+namespace Vodamep.Data.Dummy
+{
+    internal static class AgpDataGeneratorAgpReportExtensions
+    {
+        public static Person AddDummyPerson(this AgpReport report)
+        {
+            var p = AgpDataGenerator.Instance.CreatePerson();
+            report.AddPerson(p);
+            return p;
+        }
+
+        public static Person[] AddDummyPersons(this AgpReport report, int count)
+        {
+            var p = AgpDataGenerator.Instance.CreatePersons(count).ToArray();
+            report.AddPersons(p);
+            return p;
+        }
+
+        public static Staff AddDummyStaff(this AgpReport report)
+        {
+            var s = AgpDataGenerator.Instance.CreateStaff(report);
+
+            report.Staffs.Add(s);
+            return s;
+        }
+
+        public static Staff[] AddDummyStaffs(this AgpReport report, int count)
+        {
+            var s = AgpDataGenerator.Instance.CreateStaffs(report, count).ToArray();
+            report.Staffs.AddRange(s);
+            return s;
+        }
+
+        //public static Activity AddDummyActivity(this HkpvReport report, string code, DateTime? date = null)
+        //{
+        //    if (!report.Persons.Any())
+        //        report.AddDummyPerson();
+
+        //    if (!report.Staffs.Any())
+        //        report.AddDummyStaff();
+
+        //    var a = new Activity()
+        //    {
+        //        PersonId = report.Persons[0].Id,
+        //        StaffId = report.Staffs[0].Id,
+        //        DateD = date ?? report.FromD
+        //    };
+
+        //    a.Entries.AddRange(code.Split(',').Select(x => (ActivityType)int.Parse(x)).OrderBy(x => x));
+
+        //    report.Activities.Add(a);
+
+        //    return a;
+        //}
+
+        //public static Activity AddDummyConsultation(this HkpvReport report, string code, DateTime? date = null)
+        //{
+        //    if (!report.Staffs.Any())
+        //        report.AddDummyStaff();
+
+        //    var a = new Activity()
+        //    {
+        //        StaffId = report.Staffs[0].Id,
+        //        DateD = date ?? report.FromD
+        //    };
+
+        //    a.Entries.AddRange(code.Split(',').Select(x => (ActivityType)int.Parse(x)).OrderBy(x => x));
+
+
+        //    return a;
+        //}
+
+        public static Activity[] AddDummyActivities(this AgpReport report)
+        {
+            if (report.FromD == null)
+                report.FromD = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).AddMonths(-1);
+
+            if (report.ToD == null)
+                report.ToD = report.FromD.LastDateInMonth();
+
+            if (report.Staffs.Count == 0)
+                report.AddDummyStaff();
+
+            if (report.Persons.Count == 0)
+                report.AddDummyPerson();
+
+            var a = AgpDataGenerator.Instance.CreateActivities(report);
+            report.Activities.AddRange(a);
+            return a;
+        }
+
+    }
+}
---
+++ tests/Vodamep.Agp.Specs/StepDefinitions/AgpValidationSteps.cs
@@ -0,0 +1,129 @@
+﻿using FluentValidation;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using TechTalk.SpecFlow;
+using Vodamep.Data;
+using Vodamep.Data.Dummy;
+using Vodamep.Agp.Model;
+using Vodamep.Agp.Validation;
+using Xunit;
+
+namespace Vodamep.Specs.StepDefinitions
+{
+
+    [Binding]
+    public class AgpValidationSteps
+    {
+
+        private AgpReportValidationResult _result;
+        private Activity _dummyActivities;
+
+        // todo
+
+        public AgpValidationSteps()
+        {
+        }
+
+        public AgpReport Report { get; private set; }
+
+        public AgpReportValidationResult Result
+        {
+            get
+            {
+                if (_result == null)
+                {
+                    _result = (AgpReportValidationResult)Report.Validate();
+                }
+
+                return _result;
+            }
+        }
+
+
+        [Given(@"eine Meldung ist korrekt befüllt")]
+        public void GivenAValidReport()
+        {
+            // nichts zu tun
+        }
+
+        [Given(@"die Eigenschaft '(\w*)' von '(\w*)' ist auf '(.*)' gesetzt")]
+        public void GivenThePropertyIsSetTo(string name, string type, string value)
+        {
+            if (type == nameof(AgpReport))
+                this.Report.SetValue(name, value);
+            else if (type == nameof(Person))
+                this.Report.Persons[0].SetValue(name, value);
+            else if (type == nameof(Staff))
+                this.Report.Staffs[0].SetValue(name, value);
+            //else if (type == nameof(Employment))
+            //    this.Report.Staffs[0].Employments[0].SetValue(name, value);
+            else if (type == nameof(Activity))
+                foreach (var a in this.Report.Activities)
+                    a.SetValue(name, value);
+
+            else
+                throw new NotImplementedException();
+        }
+
+        [Given(@"der Id einer Person ist nicht eindeutig")]
+        public void GivenPersonIdNotUnique()
+        {
+            var p0 = this.Report.Persons[0];
+
+            var p = this.Report.AddDummyPerson();
+
+            p.Id = p0.Id;
+            p.Id = p0.Id;
+        }
+
+        [Given(@"der Id einer Mitarbeiterin ist nicht eindeutig")]
+        public void GivenStaffIdNotUnique()
+        {
+            var s0 = this.Report.Staffs[0];
+
+            var s = this.Report.AddDummyStaff();
+
+            s.Id = s0.Id;
+        }
+
+        [Then(@"*enthält (das Validierungsergebnis )?keine Fehler")]
+        public void ThenTheResultContainsNoErrors(string dummy)
+        {
+            Assert.True(this.Result.IsValid);
+            Assert.Empty(this.Result.Errors.Where(x => x.Severity == Severity.Error));
+        }
+
+        [Then(@"*enthält (das Validierungsergebnis )?keine Warnungen")]
+        public void ThenTheResultContainsNoWarnings(string dummy)
+        {
+            Assert.Empty(this.Result.Errors.Where(x => x.Severity == Severity.Warning));
+        }
+
+        [Then(@"*enthält (das Validierungsergebnis )?genau einen Fehler")]
+        public void ThenTheResultContainsOneError(object test)
+        {
+            Assert.False(this.Result.IsValid);
+            Assert.Single(this.Result.Errors.Where(x => x.Severity == Severity.Error).Select(x => x.ErrorMessage).Distinct());
+        }
+
+        [Then(@"die Fehlermeldung lautet: '(.*)'")]
+        public void ThenTheResultContainsJust(string message)
+        {
+            Assert.Equal(message, this.Result.Errors.Select(x => x.ErrorMessage).Distinct().Single());
+        }
+
+        [Then(@"enthält das Validierungsergebnis den Fehler '(.*)'")]
+        public void ThenTheResultContainsAnError(string message)
+        {
+            var pattern = new Regex(message, RegexOptions.IgnoreCase);
+
+            Assert.NotEmpty(this.Result.Errors.Where(x => x.Severity == Severity.Error && pattern.IsMatch(x.ErrorMessage)));
+        }
+
+    }
+}
