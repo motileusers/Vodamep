fff25ac9ec6d5122df72c9d527cb6dd163bae095
Markus Vinzenz <mv1@alpencode.net>
2021-02-22 19:08:55 +0100

    Differ

--- src/Vodamep/Agp/AgpReportDiffer.cs
+++ src/Vodamep/Agp/AgpReportDiffer.cs
@@ -1,31 +1,218 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Google.Protobuf.Collections;
 using Vodamep.Agp.Model;
 using Vodamep.ReportBase;
 
 namespace Vodamep.Agp
 {
-    internal class AgpReportDiffer
+    internal class AgpReportDiffer : ReportDifferBase
     {
-        public List<DiffObject> DiffList(AgpReport report1, AgpReport report2)
+
+        public AgpReportDiffer()
         {
-            var result = new List<DiffObject>();
+            this.DiffFunctions.Add(typeof(RepeatedField<Mkkp.Model.Person>), this.DiffPersons);
+            this.DiffFunctions.Add(typeof(RepeatedField<Mkkp.Model.Staff>), this.DiffStaffs);
+            this.DiffFunctions.Add(typeof(RepeatedField<Mkkp.Model.Activity>), this.DiffActivities);
+        }
 
-            //result.Add(this.FindChangedActivity(report1, report2));
-            //result.Add(this.DiffStaffActivity(report1, report2));
-            //result.Add(this.DiffPersonActivity(report1, report2));
-            //result.Add(this.DiffEmployments(report1, report2));
+        private IEnumerable<DiffObject> DiffStaffs(object obj1, object obj2)
+        {
+            var staffs1 = (obj1 as RepeatedField<Mkkp.Model.Staff>);
+            var staffs2 = (obj2 as RepeatedField<Staff>);
 
-            //result.AddRange(this.FindAddPersons(report1, report2));
-            //result.AddRange(this.FindChangedPersons(report1, report2));
-            //result.AddRange(this.FindMissingPersons(report1, report2));
+            return this.DiffItems(staffs1, staffs2, DifferenceIdType.Staff);
+        }
 
-            //result.AddRange(this.FindAddStaff(report1, report2));
-            //result.AddRange(this.FindChangedStaff(report1, report2));
-            //result.AddRange(this.FindMissingStaffs(report1, report2));
+        private IEnumerable<DiffObject> DiffPersons(object obj1, object obj2)
+        {
+            var persons1 = (obj1 as RepeatedField<Mkkp.Model.Person>);
+            var persons2 = (obj2 as RepeatedField<Person>);
 
-            //result.RemoveAll(x => x.Difference == Difference.Unchanged);
+            return DiffItems(persons1, persons2, DifferenceIdType.Person);
+        }
+
+        private IEnumerable<DiffObject> DiffActivities(object obj1, object obj2)
+        {
+
+            var activities1 = obj1 as RepeatedField<Activity>;
+            var activities2 = obj2 as RepeatedField<Activity>;
+
+            var list = new List<DiffObject>
+            {
+                this.DiffActivities(activities1, activities2),
+
+                this.DiffStaffActivity(activities1, activities2),
+                this.DiffPersonActivity(activities1, activities2),
+            };
+
+            return list;
+        }
+
+        private DiffObject DiffActivities(RepeatedField<Activity> activities1, RepeatedField<Activity> activities2)
+        {
+
+            bool isChanged = false;
+
+            for (int i = 0; i < activities1.Count; i++)
+            {
+                var activity = activities1[i];
+                var otherActivity = activities2.FirstOrDefault(x => x.DateD == activity.DateD &&
+                                                                    x.PersonId == activity.PersonId &&
+                                                                    x.StaffId == activity.StaffId);
+
+                if (otherActivity == null)
+                {
+                    isChanged = true;
+                    break;
+                }
+
+                isChanged |= !activity.Entries.SequenceEqual(otherActivity.Entries);
+
+            }
+
+            for (int i = 0; i < activities2.Count; i++)
+            {
+                var activity = activities2[i];
+                var otherActivity = activities1.FirstOrDefault(x => x.DateD == activity.DateD &&
+                                                                    x.PersonId == activity.PersonId &&
+                                                                    x.StaffId == activity.StaffId);
+
+                if (otherActivity == null)
+                {
+                    isChanged = true;
+                    break;
+                }
+
+                isChanged |= !activity.Entries.SequenceEqual(otherActivity.Entries);
+
+            }
+
+
+            return
+                new DiffObject
+                {
+                    Section = Section.Summary,
+                    DifferenceId = DifferenceIdType.Activity,
+                    Order = 0,
+                    //Difference = isChanged ? Difference.Difference : Difference.Unchanged,
+                    Difference = Difference.Unchanged
+
+                };
+        }
+
+        private DiffObject DiffStaffActivity(RepeatedField<Activity> activities1, RepeatedField<Activity> activities2)
+        {
+            var result = new DiffObject();
+            var sum1 = 0;
+            var sum2 = 0;
+
+            result.Section = Section.Summary;
+            result.DifferenceId = DifferenceIdType.StaffActivity;
+            result.Order = 1;
+            result.Type = typeof(int);
+
+            var isEntryTypeChanged = false;
+
+            foreach (var activity in activities1.Where(x => !string.IsNullOrWhiteSpace(x.StaffId)))
+            {
+                sum1 += activity.Entries.Count;
+
+                var otherActivity = activities2.FirstOrDefault(x => x.StaffId == activity.StaffId && x.DateD == activity.DateD);
+                if (otherActivity != null)
+                {
+                    isEntryTypeChanged |= this.AreChanged(activity.Entries, otherActivity.Entries);
+                }
+            }
+
+            foreach (var activity in activities2.Where(x => !string.IsNullOrWhiteSpace(x.StaffId)))
+            {
+                sum2 += activity.Entries.Count;
+
+                var otherActivity = activities1.FirstOrDefault(x => x.StaffId == activity.StaffId && x.DateD == activity.DateD);
+                if (otherActivity != null)
+                {
+                    isEntryTypeChanged |= this.AreChanged(activity.Entries, otherActivity.Entries);
+                }
+            }
+
+            result.Difference = !isEntryTypeChanged && sum1 == sum2 ? Difference.Unchanged : Difference.Difference;
+            result.Value1 = sum1;
+            result.Value2 = sum2;
 
             return result;
         }
+
+        private DiffObject DiffPersonActivity(RepeatedField<Activity> activities1, RepeatedField<Activity> activities2)
+        {
+            var result = new DiffObject();
+            var sum1 = 0;
+            var sum2 = 0;
+
+            result.Section = Section.Summary;
+            result.DifferenceId = DifferenceIdType.PersonActivity;
+            result.Order = 2;
+            result.Type = typeof(int);
+
+            var isEntryTypeChanged = false;
+
+            foreach (var activity in activities1.Where(x => !string.IsNullOrWhiteSpace(x.PersonId)))
+            {
+                sum1 += activity.Entries.Count;
+
+                var otherActivity = activities2.FirstOrDefault(x => x.PersonId == activity.PersonId && x.DateD == activity.DateD);
+                if (otherActivity != null)
+                {
+                    isEntryTypeChanged |= this.AreChanged(activity.Entries, otherActivity.Entries);
+                }
+            }
+
+            foreach (var activity in activities2.Where(x => !string.IsNullOrWhiteSpace(x.PersonId)))
+            {
+                sum2 += activity.Entries.Count;
+
+                var otherActivity = activities1.FirstOrDefault(x => x.PersonId == activity.PersonId && x.DateD == activity.DateD);
+                if (otherActivity != null)
+                {
+                    isEntryTypeChanged |= this.AreChanged(activity.Entries, otherActivity.Entries);
+                }
+            }
+
+            result.Difference = !isEntryTypeChanged && sum1 == sum2 ? Difference.Unchanged : Difference.Difference;
+            result.Value1 = sum1;
+            result.Value2 = sum2;
+
+            return result;
+        }
+
+        private bool AreChanged(IEnumerable<ActivityType> activities1, IEnumerable<ActivityType> activities2)
+        {
+            if (activities1 == null || activities2 == null)
+            {
+                return true;
+            }
+
+            var list1 = activities1.ToList();
+            var list2 = activities2.ToList();
+
+            if (list1.Count != list2.Count)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                var a = list1[i].ToString();
+                var b = list2[i].ToString();
+
+                if (list1[i].ToString().CompareTo(list2[i].ToString()) != 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
--- src/Vodamep/Mkkp/Model/Person.cs
+++ src/Vodamep/Mkkp/Model/Person.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using Vodamep.ReportBase;
 
 namespace Vodamep.Mkkp.Model
 {
-    public partial class Person
+    public partial class Person : IItem
     {
         public DateTime BirthdayD { get => this.Birthday.AsDate(); set => this.Birthday = value.AsTimestamp(); }
 
