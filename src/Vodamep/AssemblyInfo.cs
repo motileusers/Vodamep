c3e7b55469ca47245ad38bef85bf482e0d3ae119
Markus Vinzenz <mv1@alpencode.net>
2021-03-30 12:40:51 +0200

    StatLp: CM Validations

--- src/Vodamep/AssemblyInfo.cs
+++ src/Vodamep/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 [assembly: InternalsVisibleTo("Vodamep.Tests")]
 [assembly: InternalsVisibleTo("Vodamep.Specs")]
 [assembly: InternalsVisibleTo("Vodamep.Agp.Specs")]
+[assembly: InternalsVisibleTo("Vodamep.Cm.Specs")]
 [assembly: InternalsVisibleTo("Vodamep.Hkpv.Specs")]
 [assembly: InternalsVisibleTo("Vodamep.Mkkp.Specs")]
 [assembly: InternalsVisibleTo("Vodamep.StatLp.Specs")]
