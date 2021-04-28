9b5ee1bdd3e54c2c25aabd41a2e67b3978c738ef
Markus Vinzenz <mv1@alpencode.net>
2021-04-28 20:22:09 +0200

    Add Datetimes for TimeStamps

---
+++ src/Vodamep/StatLp/Model/Admission.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Vodamep.StatLp.Model
+{
+    public partial class Admission
+    {
+        public DateTime ValidD { get => this.Valid.AsDate(); set => this.Valid = value.AsTimestamp(); }
+    }
+}
---
+++ src/Vodamep/StatLp/Model/Stay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vodamep.StatLp.Model
+{
+    public partial class Stay
+    {
+        public DateTime FromD { get => this.From.AsDate(); set => this.From = value.AsTimestamp(); }
+
+        public DateTime ToD { get => this.To.AsDate(); set => this.To = value.AsTimestamp(); }
+
+    }
+}
