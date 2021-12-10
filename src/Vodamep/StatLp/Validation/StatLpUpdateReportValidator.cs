871e34ae18a30ce79ddcfab12a9f159ef235d197
dgorbach <daniel.gorbach@gmail.com>
2021-12-10 14:56:32 +0100

    StatLpUpdateReportValidator,, All Green

---
+++ src/Vodamep/StatLp/Validation/StatLpUpdateReportValidator.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Linq;
+using System.Threading;
+using Vodamep.StatLp.Model;
+
+namespace Vodamep.StatLp.Validation
+{
+    internal class StatLpUpdateReportValidator : AbstractValidator<(StatLpReport OldReport, StatLpReport Report)>
+    {
+        private static readonly DisplayNameResolver DisplayNameResolver = new DisplayNameResolver();
+
+        static StatLpUpdateReportValidator()
+        {
+            var isGerman = Thread.CurrentThread.CurrentCulture.Name.StartsWith("de", StringComparison.CurrentCultureIgnoreCase);
+            if (isGerman)
+            {
+                var loc = new DisplayNameResolver();
+                ValidatorOptions.DisplayNameResolver = (type, memberInfo, expression) => loc.GetDisplayName(memberInfo?.Name);
+            }
+        }
+
+        public StatLpUpdateReportValidator()
+        {
+            this.RuleFor(x => x).Custom((data, ctx) =>
+            {
+                foreach (var personId in data.OldReport.Persons.Select(x => x.Id))
+                {
+                    var oldValue = data.OldReport.Attributes
+                        .Where(x => x.PersonId == personId)
+                        .Where(x => x.ValueCase == Model.Attribute.ValueOneofCase.CareAllowance)
+                        .Select(x => (int)x.CareAllowance)
+                        .LastOrDefault();
+
+                    var newValue = data.Report.Attributes
+                        .Where(x => x.PersonId == personId)
+                        .Where(x => x.ValueCase == Model.Attribute.ValueOneofCase.CareAllowance)
+                        .Select(x => (int)x.CareAllowance)
+                        .LastOrDefault();
+
+                    if (oldValue >= (int)CareAllowance.L1 && oldValue <= (int)CareAllowance.L7
+                        && newValue >= (int)CareAllowance.L1 && newValue <= (int)CareAllowance.L7
+                        && Math.Abs(oldValue - newValue) > 1)
+                    {
+                        var attribute = data.Report.Attributes
+                            .Where(x => x.PersonId == personId)
+                            .Where(x => x.ValueCase == Model.Attribute.ValueOneofCase.CareAllowance)
+                            .Last();
+
+                        var index = data.Report.Attributes.IndexOf(attribute);
+
+                        ctx.AddFailure(new ValidationFailure($"{nameof(StatLpReport.Attributes)}[{index}]",
+                            $"Die Pflegestufe von '{data.Report.GetPersonName(personId)}' hat sich deutlich von '{DisplayNameResolver.GetDisplayName(((CareAllowance)oldValue).ToString())}' auf '{DisplayNameResolver.GetDisplayName(((CareAllowance)newValue).ToString())}' verändert.")
+                        {
+                            Severity = Severity.Warning
+                        });
+                    }
+
+                }
+            });
+        }
+    }
+}
