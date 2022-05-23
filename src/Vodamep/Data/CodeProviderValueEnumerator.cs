ffdfdde370b1bf5fc8c06edb1ca63ef614a823d2
Gerhard Allgäuer <Gerhard.Allgaeuer@mia-systems.at>
2022-05-23 23:58:37 +0200

    Csv Werte korrigiert und Prüf Methode

---
+++ src/Vodamep/Data/CodeProviderValueEnumerator.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using System.Diagnostics;
+
+namespace Vodamep.Data
+{
+
+
+    /// <summary>
+    /// Werte der Code Provider auslesen
+    /// </summary>
+    public class CodeProviderValueEnumerator
+    {
+
+        public void Test()
+        {
+
+
+            List<CodeProviderValue> result = new List<CodeProviderValue>();
+
+
+            // Auflisten der Code Provider
+
+            List<Type> codeProviderTypes = Assembly.GetAssembly(typeof(CodeProviderBase))
+                                                   .GetTypes()
+                                                   .Where(TheType => TheType.IsClass && !TheType.IsAbstract && TheType.IsSubclassOf(typeof(CodeProviderBase)))
+                                                   .ToList();
+
+            Dictionary<string, string> textDictionary = new Dictionary<string, string>();
+
+            foreach (Type codeProviderType in codeProviderTypes)
+            {
+                // die 2 sind keine Enum Provider, da gibts nur ungewollte Überschneidungen
+                if (codeProviderType != typeof(CountryCodeProvider) &&
+                    codeProviderType != typeof(Postcode_CityProvider) &&
+                    codeProviderType != typeof(Vodamep.Data.Hkpv.Postcode_CityProvider) &&
+                    codeProviderType != typeof(InsuranceCodeProvider))
+                {
+                    CodeProviderBase baseProvider = Activator.CreateInstance(codeProviderType) as CodeProviderBase;
+
+                    IReadOnlyDictionary<string, string> values = baseProvider.Values;
+
+                    foreach (KeyValuePair<string, string> key in values)
+                    {
+                        if (!textDictionary.ContainsKey(key.Key))
+                        {
+                            textDictionary.Add(key.Key, key.Value);
+                        }
+                        else
+                        {
+                            // doppelte Keys, das ist nicht so schön
+                        }
+                    }
+                }
+
+            }
+
+
+            // Auflisten der Enums aus den Proto Definitionen
+
+            List<Type> reflectionTypes = Assembly.GetAssembly(typeof(CodeProviderBase))
+                                  .GetTypes()
+                                  .Where(p => p.Name.EndsWith("Reflection"))
+                                  .ToList();
+
+
+
+            foreach (Type messageType in reflectionTypes)
+            {
+                PropertyInfo[] properties = messageType.GetProperties(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+                foreach (PropertyInfo property in properties)
+                {
+                    if (property.Name == "Descriptor")
+                    {
+                        FileDescriptor fileDescriptor = property.GetValue(messageType, null) as FileDescriptor;
+                        IList<EnumDescriptor> enumDescriptors = fileDescriptor.EnumTypes;
+
+                        foreach (EnumDescriptor enumDescriptor in enumDescriptors)
+                        {
+                            // Für die hier gibt's eine Beschreibungstexte
+                            if (enumDescriptor.ClrType != typeof(Vodamep.Hkpv.Model.ActivityType))
+                            {
+
+                                Type enumClrType = enumDescriptor.ClrType;
+                                List<string> names = Enum.GetNames(enumClrType).ToList();
+
+
+                                IList<EnumValueDescriptor> enumDescriptorValues = enumDescriptor.Values;
+                                foreach (EnumValueDescriptor enumDescriptorValue in enumDescriptorValues)
+                                {
+
+                                    CodeProviderValue codeProviderValue = new CodeProviderValue()
+                                    {
+                                        ReportType = messageType,
+                                        EnumType = enumClrType,
+                                        EnumValue = names[enumDescriptorValue.Index],
+                                        ProtoValue = enumDescriptorValue.Name,
+                                        Text = "",
+                                    };
+
+                                    if (textDictionary.ContainsKey(codeProviderValue.ProtoValue))
+                                    {
+                                        codeProviderValue.Text = textDictionary[codeProviderValue.ProtoValue];
+                                    }
+                                    else
+                                    {
+                                        throw new Exception("Keinen Wert im csv gefunden.");
+                                    }
+
+                                    result.Add(codeProviderValue);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ src/Vodamep/Data/MainAttendanceClosenessProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Vodamep.Data
+{
+    public class MainAttendanceClosenessProvider : CodeProviderBase
+    {
+        private static volatile MainAttendanceClosenessProvider instance;
+        private static object syncRoot = new Object();
+
+        public static MainAttendanceClosenessProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new MainAttendanceClosenessProvider();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public override string Unknown => "";
+
+        protected override string ResourceName => "Datasets.main_attendance_closeness.csv";
+    }
+}
