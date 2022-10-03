c85cfd6843f45c20edf8aeff7381344da62a23b6
Gerhard Allgäuer <gerhard.allgaeuer@connexia.at>
2022-10-03 21:21:40 +0200

    Mohi + Tb Service Provider

---
+++ src/Vodamep/Data/Mohi/ServiceProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Google.Protobuf.Reflection;
+
+namespace Vodamep.Data.Mohi
+{
+    public class ServiceProvider : CodeProviderBase
+    {
+        private static volatile ServiceProvider instance;
+        private static object syncRoot = new Object();
+
+        public static ServiceProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new ServiceProvider();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        protected override FileDescriptor Descriptor => Vodamep.Mohi.Model.MohiReflection.Descriptor;
+
+        public override string Unknown => "";
+
+        protected override string ResourceName => "Datasets.Mohi.Service.csv";
+    }
+}
---
+++ src/Vodamep/Data/Tb/ServiceProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Google.Protobuf.Reflection;
+
+namespace Vodamep.Data.Tb
+{
+    public class ServiceProvider : CodeProviderBase
+    {
+        private static volatile ServiceProvider instance;
+        private static object syncRoot = new Object();
+
+        public static ServiceProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new ServiceProvider();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        protected override FileDescriptor Descriptor => Vodamep.Tb.Model.TbReflection.Descriptor;
+
+        public override string Unknown => "";
+
+        protected override string ResourceName => "Datasets.Tb.Service.csv";
+    }
+}
