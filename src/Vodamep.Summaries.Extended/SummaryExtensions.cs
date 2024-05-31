b60dda56a967bf2a04bd099daa4a0cbb05a719ec
dgorbach <daniel.gorbach@gmail.com>
2024-05-31 10:42:19 +0200

    Extended

---
+++ src/Vodamep.Summaries.Extended/SummaryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Markdig;
+
+namespace Vodamep.Summaries
+{
+    public static class SummaryExtensions
+    {
+        public static string ToHtml(this Summary summary)
+        {
+            return Markdown.ToHtml(summary.Text);
+        }
+
+        public static string Diff(this Summary oldSummary, Summary newSummary) { 
+        
+            var oldHtml = oldSummary.ToHtml();
+            var newHtml = newSummary.ToHtml();
+
+
+            var diffHelper = new HtmlDiff.HtmlDiff(oldHtml, newHtml);
+            string diffOutput = diffHelper.Build();
+
+            return diffOutput;
+
+        }
+
+    }
+}
