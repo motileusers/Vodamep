052f423ee03b8c174df3eeaa1e465d419ccb0096
dgorbach <daniel.gorbach@gmail.com>
2024-06-03 13:30:07 +0200

    ToHtml mit Styles

--- src/Vodamep.Summaries.Extended/SummaryExtensions.cs
+++ src/Vodamep.Summaries.Extended/SummaryExtensions.cs
@@ -4,23 +4,53 @@ namespace Vodamep.Summaries
 {
     public static class SummaryExtensions
     {
-        public static string ToHtml(this Summary summary)
+        public static string ToHtml(this Summary summary, bool includeStyles = false)
         {
-            return Markdown.ToHtml(summary.Text);
+            var pipeline = new MarkdownPipelineBuilder()
+                .UseAdvancedExtensions()
+                .Build();
+
+            var content = Markdown.ToHtml(summary.Text, pipeline);
+
+            return includeStyles ? WithTemplate(content) : content;
         }
 
-        public static string Diff(this Summary oldSummary, Summary newSummary) { 
-        
+        private static string WithTemplate(string htmlContent)
+        {
+            var template = GetTempate();
+
+            return template.Replace("<!-- content -->", htmlContent);
+        }
+
+        private static string GetTempate()
+        {
+            var templateStream = typeof(SummaryExtensions).Assembly.GetManifestResourceStream("Vodamep.Summaries.Template.html");
+            var stylesSteam = typeof(SummaryExtensions).Assembly.GetManifestResourceStream("Vodamep.Summaries.Styles.css");
+
+            if (templateStream == null || stylesSteam == null)
+            {
+                return string.Empty;
+            }
+
+            using var templateReader = new StreamReader(templateStream);
+            using var stylesReader = new StreamReader(stylesSteam);
+
+            var template = templateReader.ReadToEnd();
+            var style = stylesReader.ReadToEnd();
+
+            return template.Replace("<!-- style -->", $"<style>{style}</style>");
+        }
+
+        public static string Diff(this Summary oldSummary, Summary newSummary, bool includeStyles = false)
+        {
             var oldHtml = oldSummary.ToHtml();
             var newHtml = newSummary.ToHtml();
 
+            var diff = new HtmlDiff.HtmlDiff(oldHtml, newHtml);
 
-            var diffHelper = new HtmlDiff.HtmlDiff(oldHtml, newHtml);
-            string diffOutput = diffHelper.Build();
+            string diffOutput = diff.Build();
 
-            return diffOutput;
-
+            return includeStyles ? WithTemplate(diffOutput) : diffOutput;
         }
-
     }
 }
--- tests/Vodamep.Summaries.Tests/SummaryRegistryTests.cs
+++ tests/Vodamep.Summaries.Tests/SummaryRegistryTests.cs
@@ -12,20 +12,7 @@ namespace Vodamep.Summaries.Tests
         {
             _registry = SummaryRegistry.CreateDefault();
 
-            _report = new MkkpReport
-            {
-                FromD = DateTime.Today,
-                ToD = DateTime.Today
-            };
-
-            _report.Persons.Add(new Person { Id = "1", FamilyName = "Family", GivenName = "Given1" });
-            _report.Persons.Add(new Person { Id = "2", FamilyName = "Family", GivenName = "Given2" });
-
-            _report.Activities.Add(new Activity { PersonId = "1", DateD = DateTime.Today, ActivityScope = ActivityScope.PalliativeCareScope, Minutes = 100 });
-
-            _report.Activities.Add(new Activity { PersonId = "1", DateD = DateTime.Today, ActivityScope = ActivityScope.ChildCareScope, Minutes = 10 });
-
-            _report.Activities.Add(new Activity { PersonId = "2", DateD = DateTime.Today, ActivityScope = ActivityScope.ChildCareScope, Minutes = 200 });
+            _report = CreateReport();
         }
 
         [Fact]
@@ -66,7 +53,72 @@ namespace Vodamep.Summaries.Tests
 
             Assert.NotNull(summary);
             Assert.NotEmpty(summary.Text);
-
         }
+
+        [Fact]
+        public async Task ToHtml_IncludeStyles_ContainsStyles()
+        {
+            var summaryDescription = SummaryFactory.GetDescription();
+
+            var summary = await _registry.CreateSummary(summaryDescription, _report);
+
+            var html = summary!.ToHtml(true);
+
+            Assert.Contains("<style>", html);
+        }
+
+        [Fact]
+        public async Task ToHtml_NotIncludeStyles_ContainsStyles()
+        {
+            var summaryDescription = SummaryFactory.GetDescription();
+
+            var summary = await _registry.CreateSummary(summaryDescription, _report);
+
+            var html = summary!.ToHtml(false);
+
+            Assert.DoesNotContain("<style>", html);
+        }
+
+        [Fact]
+        public async Task Diff_BirthdayIsAdded_BirthdayIsMarkedAsINS()
+        {
+            var summaryDescription = SummaryFactory.GetDescription();
+
+            _report.Persons[0].Birthday = null;
+
+            var summary = await _registry.CreateSummary(summaryDescription, _report);
+
+            var r2 = _report.Clone();
+
+            r2.Persons[0].BirthdayD = new DateTime(2024, 6, 3);
+
+            var summary2 = await _registry.CreateSummary(summaryDescription, r2);
+
+            var diff = summary!.Diff(summary2!, true);
+
+            Assert.Contains("<ins class='diffins'>03.06.2024</ins>", diff);
+        }
+
+
+        public static MkkpReport CreateReport()
+        {
+            var report = new MkkpReport
+            {
+                FromD = DateTime.Today,
+                ToD = DateTime.Today
+            };
+
+            report.Persons.Add(new Person { Id = "1", FamilyName = "Family", GivenName = "Given1" });
+            report.Persons.Add(new Person { Id = "2", FamilyName = "Family", GivenName = "Given2" });
+
+            report.Activities.Add(new Activity { PersonId = "1", DateD = DateTime.Today, ActivityScope = ActivityScope.PalliativeCareScope, Minutes = 100 });
+
+            report.Activities.Add(new Activity { PersonId = "1", DateD = DateTime.Today, ActivityScope = ActivityScope.ChildCareScope, Minutes = 10 });
+
+            report.Activities.Add(new Activity { PersonId = "2", DateD = DateTime.Today, ActivityScope = ActivityScope.ChildCareScope, Minutes = 200 });
+
+            return report;
+        }
+
     }
 }
