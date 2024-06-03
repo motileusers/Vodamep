32993e18a7b327e18b0893e0864f3895cb938c0c
dgorbach <daniel.gorbach@gmail.com>
2024-06-03 13:30:07 +0200

    ToHtml mit Styles

--- src/Vodamep.Summaries/Mkkp/SummaryFactory.cs
+++ src/Vodamep.Summaries/Mkkp/SummaryFactory.cs
@@ -33,7 +33,21 @@ namespace Vodamep.Summaries.Mkkp
             static string formatCol(string text, int len) => text.PadRight(len)[..len];
 
 
-            foreach (var person in model.Persons)
+            var headerCols = new[]
+            {
+                formatCol("Nachame", colWidth1),
+                formatCol("Vorname", colWidth1),
+                "Geburtsdatum",
+                formatCol("Plz", 4),
+                formatCol("Ort", colWidth2),
+                "Geschlecht"
+            };
+
+            sb.AppendLine($"| {string.Join(" | ", headerCols)} |");
+
+            sb.AppendLine($"| {string.Join(" | ", headerCols.Select(x => new string('-', x.Length)))} |");
+
+            foreach (var person in model.Persons.OrderBy(x => x.FamilyName).ThenBy(x => x.GivenName))
             {
                 var columns = new[]
                 {
@@ -44,9 +58,6 @@ namespace Vodamep.Summaries.Mkkp
                     formatCol(person.City, colWidth2),
                     formatCol($"{person.Gender.Format()}", 4)
                 };
-
-
-
 
                 sb.AppendLine($"| {string.Join(" | ", columns)} |");
             }
@@ -59,16 +70,32 @@ namespace Vodamep.Summaries.Mkkp
 
             static string formatCol(string text, int len) => text.PadRight(len)[..len];
 
+            var headerCols = new[]
+            {
+                formatCol("Name", 2*colWidth1),
+                formatCol("HospitalDoctor", colWidth1),
+                formatCol("LocalDoctor", colWidth1),
+                formatCol("Versicherung", colWidth1),
+                formatCol("Referrer", colWidth1),
+                formatCol("OtherReferrer", colWidth1),
+                formatCol("CareAllowance", colWidth1),
+                formatCol("Diagnoses", colWidth2)
+            };
 
-            foreach (var person in model.Persons)
+            sb.AppendLine($"| {string.Join(" | ", headerCols)} |");
+
+            sb.AppendLine($"| {string.Join(" | ", headerCols.Select(x => new string('-', x.Length)))} |");
+
+            foreach (var person in model.Persons.OrderBy(x => x.FamilyName).ThenBy(x => x.GivenName))
             {
                 var columns = new[]
                 {
+                    formatCol($"{person.FamilyName} {person.GivenName}", 2*colWidth1),
                     formatCol(person.HospitalDoctor, colWidth1),
-                    formatCol(person.Insurance, colWidth1),
                     formatCol(person.LocalDoctor, colWidth1),
-                    formatCol(person.OtherReferrer, colWidth1),
+                    formatCol(person.Insurance, colWidth1),
                     formatCol($"{person.Referrer}", colWidth1),
+                    formatCol(person.OtherReferrer, colWidth1),
                     formatCol($"{person.CareAllowance}", colWidth1),
                     formatCol($"{person.Diagnoses}", colWidth1),
                 };
