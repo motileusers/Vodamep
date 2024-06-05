3e0477c0e6878d26887e755296f23d6446705995
Daniel Gorbach <daniel@motus-software.at>
2024-06-05 09:30:34 +0200

    Anfang Hkpv

---
+++ src/Vodamep.Summaries/Hkpv/HkpvExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Vodamep.Hkpv.Model;
+
+namespace Vodamep.Summaries.Hkpv
+{
+    //todo: Überlegen wohin. Diese Extensions könnten auch in die Vodamep-Assembly. Vielleicht sind sie bereis dort?
+    public static class HkpvExtensions
+    {
+        public static string Format(this Gender gender) => gender switch
+        {
+            Gender.Female => "w",
+            Gender.Male => "m",
+            Gender.Open => "o",
+            Gender.Divers => "div",
+            Gender.Inter => "inter",
+            _ => string.Empty
+        };
+    }
+}
